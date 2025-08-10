81df1f61522651946f8f35929a18711cb8f60d33
kn1ghtm0nster <diego.quintanilla@proton.me>
2025-08-10 17:28:21 -0400

    add Author model

--- core/models.py
+++ core/models.py
@@ -12,3 +12,14 @@ class Person(models.Model):
 
     def __str__(self):
         return f"<Person {self.first_name} {self.last_name}>"
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+    bio = models.TextField(blank=True, null=True)
+    website = models.URLField(blank=True, null=True)
+    email = models.EmailField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"<Author {self.name}>"
